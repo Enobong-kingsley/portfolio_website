23b96c28464b89454a4f968e348c68a88bd1c1e8
Enobong-kingsley <enobongndedde@gmail.com>
2023-04-29 21:40:20 +0100

    initial_commit

--- lib/presentation/home/home_controller.dart
+++ lib/presentation/home/home_controller.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:miladjalali_ir/presentation/blog/blog_page.dart';
+
 import 'package:miladjalali_ir/presentation/experiences/experiences_binding.dart';
 import 'package:miladjalali_ir/presentation/information/information_binding.dart';
 import 'package:miladjalali_ir/presentation/information/information_page.dart';
 import '../../app/constants/themes.dart';
 import '../about/about_binding.dart';
 import '../about/about_page.dart';
-import '../blog/blog_binding.dart';
+
 import '../experiences/experiences_page.dart';
 import '../information/information_controller.dart';
-import '../projects/projects_page.dart';
-import '../projects/projets_binding.dart';
+
 
 class HomeController extends GetxController {
 
--- lib/presentation/projects/projets_binding.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:get/get.dart';
-// import 'projects_controller.dart';
-
-// class ProjectsBinding extends Bindings {
-//   @override
-//   void dependencies() {
-//     Get.put(ProjectsController());
-//   }
-// }
